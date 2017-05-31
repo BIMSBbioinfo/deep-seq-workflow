adc206695bcc19442e2be56f109b7c12e07bdc77
cmassimo <cmassim@mdc-berlin.de>
2017-05-31 16:27:29 +0200

    fixes a bug that prevented machine type selection when running with option: --rundir

--- lib/workflow.rb
+++ lib/workflow.rb
@@ -20,30 +20,37 @@ require_relative 'dsw/mini_seq'
 
 class Workflow
   include Errors
+
+  def self.select_manager(rundir)
+    begin
+      manager = Sequencer.select(run_dir)
+    rescue Errors::EmptyRunDirPathError => erdpe
+      puts("rundir path parameter is empty ('#{erdpe.message}'); Exiting with status 1.")
+      Mailer.notify_admins(Sequencer.new(run_dir), 'workflow_start', erdpe)
+      exit(1)
+    rescue Errors::UnknownMachineTypeError => umte
+      puts("Unknown machine type extrapolated from directory name: '#{umte.message}'; Exiting with status 1.")
+      Mailer.notify_admins(Sequencer.new(run_dir), 'workflow_start', umte)
+      exit(1)
+    end
+
+    return manager
+  end
  
   def self.start(step)
     Dir.glob(File.join(Conf.global_conf[:basecall_dir], Conf.global_conf[:seq_dir_regexp], '*'), File::FNM_PATHNAME).select {|d| File.directory?(d) }.each do |run_dir|
-      begin
-        @manager = Sequencer.select(run_dir)
-      rescue Errors::EmptyRunDirPathError => erdpe
-        puts("rundir path parameter is empty ('#{erdpe.message}'); Exiting with status 1.")
-        Mailer.notify_admins(Sequencer.new(run_dir), 'workflow_start', erdpe)
-        exit(1)
-      rescue Errors::UnknownMachineTypeError => umte
-        puts("Unknown machine type extrapolated from directory name: '#{umte.message}'; Exiting with status 1.")
-        Mailer.notify_admins(Sequencer.new(run_dir), 'workflow_start', umte)
-        exit(1)
-      end
 
-      if Dir["#{run_dir}/*"].empty? && Dir.exist?(File.join(Conf.global_conf[:basecall_dir], @manager.run_name))
-        @manager.logger.warning("#{run_dir} is empty and a rundir with the same name already exists in /data/basecalls: deleting #{run_dir} (sequencer artifacts)")
+      manager = select_manager(run_dir)
+
+      if Dir["#{run_dir}/*"].empty? && Dir.exist?(File.join(Conf.global_conf[:basecall_dir], manager.run_name))
+        manager.logger.warning("#{run_dir} is empty and a rundir with the same name already exists in /data/basecalls: deleting #{run_dir} (sequencer artifacts)")
         FileUtils.remove_dir(run_dir, true)
       else
         if step == :forbid
           # different, faster cronjob, this way we get less log noise
-          @manager.forbid!
+          manager.forbid!
         else
-          @manager.run_from(step)
+          manager.run_from(step)
         end
       end
 
