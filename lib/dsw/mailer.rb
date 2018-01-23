db1f6a1397fda404872f45e3b5f0bae78b40e2c5
Ricardo Wurmus <rekado@elephly.net>
2018-01-23 18:06:43 +0100

    Use registered email address.

--- lib/dsw/mailer.rb
+++ lib/dsw/mailer.rb
@@ -5,7 +5,7 @@ class Mailer
   def self.notify_admins(manager, op_code, error =nil)
     unless Conf.global_conf[:debug]
       ADMINS.each do |adm|
-        msg = %Q|From: deep_seq_workflow <dsw@mdc-berlin.net>
+        msg = %Q|From: deep_seq_workflow <dsw@mdc-berlin.de>
 To: #{adm}
 Subject: [Deep Seq workflow] Error: #{op_code}
 Date: #{Time.now}
@@ -27,7 +27,7 @@ See #{manager.log_file_name} for details.\n|
         msg << "\n---\ndsw"
 
         Net::SMTP.start('localhost', 25) do |smtp|
-          smtp.send_message msg, 'dsw@mdc-berlin.net', adm
+          smtp.send_message msg, 'dsw@mdc-berlin.de', adm
         end
       end
     else
@@ -40,7 +40,7 @@ See #{manager.log_file_name} for details.\n|
     unless Conf.global_conf[:debug]
       users = EVERYBODY
       users.each do |user|
-        msg = %Q|From: deep_seq_workflow <dsw@mdc-berlin.net>
+        msg = %Q|From: deep_seq_workflow <dsw@mdc-berlin.de>
 To: #{user}
 Subject: [Deep Seq workflow] Processing of run #{manager.run_name} finished
 Date: #{Time.now}
@@ -52,7 +52,7 @@ The backup procedure and demultiplexing may still be underway.
 ---\ndsw\n|
 
         Net::SMTP.start('localhost', 25) do |smtp|
-          smtp.send_message msg, 'dsw@mdc-berlin.net', user
+          smtp.send_message msg, 'dsw@mdc-berlin.de', user
         end
       end
     else
@@ -64,7 +64,7 @@ The backup procedure and demultiplexing may still be underway.
   def self.notify_run_errors(manager, recipients)
     unless Conf.global_conf[:debug]
       recipients.each do |rcp|
-        msg = %Q|From: deep_seq_workflow <dsw@mdc-berlin.net>
+        msg = %Q|From: deep_seq_workflow <dsw@mdc-berlin.de>
 To: #{rcp}
 Subject: [Deep Seq workflow] Errors during sequencing run detected
 Date: #{Time.now}
@@ -82,7 +82,7 @@ and contact the vendor if needed.
 ---\ndsw|
 
         Net::SMTP.start('localhost', 25) do |smtp|
-          smtp.send_message msg, 'dsw@mdc-berlin.net', rcp
+          smtp.send_message msg, 'dsw@mdc-berlin.de', rcp
         end
       end
     else
