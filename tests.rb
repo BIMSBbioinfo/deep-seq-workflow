b3c4c4982d0b8cbe685b1229ff4a45f4caf0e1c5
Ricardo Wurmus <rekado@elephly.net>
2020-05-07 16:15:32 +0200

    tests: Check selection of all sequencers.

--- tests.rb
+++ tests.rb
@@ -8,6 +8,19 @@ describe Sequencer do
   it 'selects the appropriate subclass from the sequencer directory' do
     expect(Sequencer.select("#{$root}/.seq_MN00157/this-is-a/run-directory")).
       to be_instance_of(MiniSeq)
+    expect(Sequencer.select("#{$root}/.seq_NB501326/this-is-alsa-a/run-directory")).
+      to be_instance_of(NextSeq)
+    expect(Sequencer.select("#{$root}/.seq_NS500455/this-is-alsa-a/run-directory")).
+      to be_instance_of(NextSeq)
+    expect(Sequencer.select("#{$root}/.seq_NS500648/this-is-alsa-a/run-directory")).
+      to be_instance_of(NextSeq)
+    expect(Sequencer.select("#{$root}/.seq_SN541/this-is-alsa-a/run-directory")).
+      to be_instance_of(HiSeq)
+    expect(Sequencer.select("#{$root}/.seq_K00302/run-dir")).
+      to be_instance_of(HiSeq)
+
+    expect{Sequencer.select("#{$root}/.seq_ISEQ100/run-dir")}.
+      to raise_error(Errors::UnknownMachineTypeError)
   end
 
   it 'selects the appropriate subclass from the run directory' do
